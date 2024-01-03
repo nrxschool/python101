acd6f5a301014172dbc5010bb1c47579362d6a56
Lucas Oliveira <olivmath@protonmail.com>
2024-01-03 15:34:52 -0300

    update: rename

--- aulas/hands-on/run.sh
+++ aulas/hands-on/run.sh
@@ -1,3 +1,10 @@
-for arquivo in *.; do
-    mv "$arquivo" "${prefixo}${arquivo}"
+#!/bin/bash
+
+# Loop pelas pastas que seguem o padrão
+for pasta in aula*-hands-on; do
+    # Extrai o número da aula do nome da pasta
+    numero=$(echo $pasta | sed 's/aula\([0-9]*\)-hands-on/\1/')
+    
+    # Renomeia a pasta
+    mv "$pasta" "aula$numero"
 done
